b90306d3f3e7fe44f1ead8cfcb4fd92dc990d1d5
xxx <1297606787@qq.com>
2019-03-06 21:53:17 +0800

    friend add completed

--- src/components/chat/sessionlist/index.js
+++ src/components/chat/sessionlist/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Dialog from '@component/common/dialog';
 import './index.css'
 
 export default class sessionList extends Component {
@@ -6,12 +7,8 @@ export default class sessionList extends Component {
     super(props);
     this.state = {
       friendList: [
-        {
-          jid: 'asemo',
-          name: 'test1',
-          subscription: 'both'
-        }
-      ]
+      ],
+      showPanel: false,
     }
     
   }
@@ -20,8 +17,48 @@ export default class sessionList extends Component {
       onOpened: ( message ) => {          //连接成功回调
         this.getRosters();
       },
+      onRoster: function ( message ) {
+        this.getRosters();
+      },
+      onPresence: (message) => {
+        this.handlePresence(message);
+    }  
      } ) 
       
+  }
+  handlePresence = (message) => {
+     //对方收到请求加为好友
+  if (message.type === 'subscribe') {
+    // 显示同意/拒绝面板
+    this.setState({
+      showPanel: true,
+    })
+    this.subscribeMessage = message;
+  }
+  }
+  agree = () => {
+    let message = this.subscribeMessage;
+    sdk.conn.subscribed({
+      to: message.from,
+      message : '[resp:true]'
+    });
+    sdk.conn.subscribe({//需要反向添加对方好友
+      to: message.from,
+      message : '[resp:true]'
+    });
+    this.setState({
+      showPanel: false
+  });
+  }
+  reject = () => {
+    let message = this.subscribeMessage;
+    sdk.conn.unsubscribe({
+      to: message.from,
+      message : 'rejectAddFriend'
+    });
+    this.setState({
+      showPanel: false
+  });
   }
   getRosters = () => [
     sdk.conn.getRoster({
@@ -40,12 +77,29 @@ export default class sessionList extends Component {
     })
   ]
   render() {
-    let {friendList} = this.state
+    let {friendList, showPanel} = this.state
+    let message = this.subscribeMessage;
     return (
       <div className="sessionlist"> 
         {friendList.length ? friendList.map((friend)=>{
           return <SessionItem friend = {friend} key = {friend.name}/>
         }): null}
+        {showPanel ? <Dialog className=""
+         title = "好友申请" 
+        content = {
+          <div>
+            <div>{message.from}邀请你加为好友</div>
+            <div>留言：{message.status}</div>
+          </div>
+          }
+        footer = {
+          <div>
+            <button className="reject" onClick = {this.reject}>拒绝</button>
+            <button className="agree" onClick = {this.agree}>同意</button> 
+          </div> 
+        }
+          />:null
+        }
       </div> 
     )
   }
--- src/components/chat/slidebar/index.js
+++ src/components/chat/slidebar/index.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 import { getToken } from "@utils/token";
 import Icon from "@component/common/Icon";
-
+import Dialog from "@component/common/dialog";
 import './index.css'
 
 export default class SliderBar extends Component {
+  state = {
+    showPanel: false
+  }
+  showAddRosterPanel = () => {
+    this.setState({
+      showPanel:true
+    })
+  }
+  addRoster = () => {
+    sdk.conn.subscribe({
+      to: this.refs.nickname.value,
+      message: '加个好友呗!'   
+  });
+    this.setState({
+      showPanel: false
+    })
+  }
     render() {
         console.log(getToken());
         let tokenUser = getToken();
         let username = tokenUser ? tokenUser.user.username : "";
+        let {showPanel} = this.state;
         return (
             <div className="slidebar">
                 <div className="profile">
@@ -26,10 +44,25 @@ export default class SliderBar extends Component {
                     </div>
                 </div>
                 <div className="footer">
-                    <div className="app-item setting">
+                    <div className="app-item setting" onClick= {this.showAddRosterPanel}>
                         <Icon type="setting" />
                     </div>
                 </div>
+                {showPanel ? <Dialog 
+                  content= {
+                    <div className = "input-container">
+                      <input type="text" ref="nickname" placeholder="输入名字"/>
+                    </div>
+                  }
+                  footer={
+                    <div className="footer">
+                      <button className="btn" onClick={this.addRoster}>确定</button>
+                    </div>
+                  }
+                  // onCLose = {1}
+                  title="添加好友">
+
+                </Dialog>: null}
             </div>
         );
     }
